307311a9bc42abd2523e01e8a671daf96f730b41
lofidewanto <lofidewanto@gmail.com>
2022-05-01 17:55:02 +0200

    Update

--- netflix-tarifrechner-management/netflix-tarifrechner-server-jpa/src/test/java/com/example/netflix/tarif/NetflixTarifrechnerRestControllerWebClientTest.java
+++ netflix-tarifrechner-management/netflix-tarifrechner-server-jpa/src/test/java/com/example/netflix/tarif/NetflixTarifrechnerRestControllerWebClientTest.java
@@ -2,6 +2,7 @@ package com.example.netflix.tarif;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,8 +21,11 @@ class NetflixTarifrechnerRestControllerWebClientTest {
     private static final String URI_TO_TEST = NetflixTarifRechnerEndpoint.TARIFRECHNER
             + "/Lofi/18";
 
+    @Disabled("Test is disabled, because it is not possible to test a WebClient without a server running")
     @Test
     void webClientAufruf() {
+        // Achtung: Die Spring Boot App muss vorher gestartet werden, damit der
+        // WebClient funktioniert!
         WebClient webClient = WebClient.create(URL_TO_TEST);
         Mono<String> result = webClient.get().uri(URI_TO_TEST).retrieve().bodyToMono(String.class);
         String body = result.block();
