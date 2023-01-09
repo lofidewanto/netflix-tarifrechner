ad02adf7bd33d9df00c396bcb45fcdff7b9f7921
lofidewanto <lofidewanto@gmail.com>
2023-01-09 22:43:47 +0100

    update

--- netflix-tarifrechner-management/netflix-tarifrechner-server-jpa/src/test/java/com/example/netflix/tarif/KundeRepositoryTest.java
+++ netflix-tarifrechner-management/netflix-tarifrechner-server-jpa/src/test/java/com/example/netflix/tarif/KundeRepositoryTest.java
@@ -5,8 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 public class KundeRepositoryTest {
@@ -17,11 +15,11 @@ public class KundeRepositoryTest {
     KundeRepository kundeRepository;
 
     @Test
-    @Transactional
-    @Rollback
     void create_TestKunde() {
         Kunde save = kundeRepository.save(new Kunde("Lofi", 20));
 
         kundeRepository.findAll().forEach(kunde -> logger.info(kunde.getName()));
+
+        kundeRepository.deleteAll();
     }
 }
--- netflix-tarifrechner-management/netflix-tarifrechner-server-jpa/src/test/java/com/example/netflix/tarif/KundeServiceTest.java
+++ netflix-tarifrechner-management/netflix-tarifrechner-server-jpa/src/test/java/com/example/netflix/tarif/KundeServiceTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class KundeServiceTest {
@@ -18,8 +16,6 @@ class KundeServiceTest {
     KundeRepository kundeRepository;
     
     @Test
-    @Transactional
-    @Rollback
     void create_Kunde() {
         Kunde createKunde1 = kundeService.createKunde(new Kunde("Max", 18));
         Kunde createKunde2 = kundeService.createKunde(new Kunde("Muster", 30));
