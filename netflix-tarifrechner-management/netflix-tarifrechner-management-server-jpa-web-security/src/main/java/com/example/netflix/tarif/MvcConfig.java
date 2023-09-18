5d18d02a36ca6e33abb06b1d5c0ac56a284c96f3
lofidewanto <lofidewanto@gmail.com>
2023-09-18 22:33:53 +0200

    Add comments

--- netflix-tarifrechner-management/netflix-tarifrechner-management-server-jpa-web-security/src/main/java/com/example/netflix/tarif/MvcConfig.java
+++ netflix-tarifrechner-management/netflix-tarifrechner-management-server-jpa-web-security/src/main/java/com/example/netflix/tarif/MvcConfig.java
@@ -2,16 +2,17 @@ package com.example.netflix.tarif;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig {
+public class MvcConfig implements WebMvcConfigurer {
 
 	// Spring Boot Auto Configuration without @EnableWebMvc
 	// This is just an example and won't be used in this web app.
     public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("greeting");
 		registry.addViewController("/").setViewName("greeting");
-		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/hello").setViewName("greeting");
 		registry.addViewController("/netflix-tarifrechner").setViewName("netflix-tarifrechner");
 		registry.addViewController("/login").setViewName("login");
 	}
