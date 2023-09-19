e7516e86da750edafac1352c180d90f2b22e04ad
lofidewanto <lofidewanto@gmail.com>
2023-09-19 16:17:27 +0200

    Add MockMvn and WebMvcTest

---
+++ netflix-tarifrechner-management/netflix-tarifrechner-management-server-jpa-web/src/test/java/com/example/netflix/tarif/GreetingWebControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.netflix.tarif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(GreetingWebController.class)
+class GreetingWebControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void greeting() throws Exception {
+        mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
+    }
+}
