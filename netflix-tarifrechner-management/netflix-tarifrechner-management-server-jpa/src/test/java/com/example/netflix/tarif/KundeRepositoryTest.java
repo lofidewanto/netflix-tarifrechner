73e7f7be6ece8653642f48507f014f3847688bb0
lofidewanto <lofidewanto@gmail.com>
2025-10-27 21:54:03 +0100

    Update test class

--- netflix-tarifrechner-management/netflix-tarifrechner-management-server-jpa/src/test/java/com/example/netflix/tarif/KundeRepositoryTest.java
+++ netflix-tarifrechner-management/netflix-tarifrechner-management-server-jpa/src/test/java/com/example/netflix/tarif/KundeRepositoryTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-public class KundeRepositoryTest {
+class KundeRepositoryTest {
 
     private final Logger logger = LoggerFactory.getLogger(KundeRepositoryTest.class);
 
